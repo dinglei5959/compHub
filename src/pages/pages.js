ee9509ceba8a9334c64cc5b9ce4d29a44e790c87
dinglei <sx-dingl@dtdream.com>
2017-12-09 20:34:27 +0800

    将遮罩放到基础组件当中

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -2,6 +2,7 @@ export default [
   {
     name: '基础性组件',
     item: [
+      { name: '遮罩', intro: 'backdrop' },
       { name: '菊花图', intro: 'spinner' },
       { name: '按钮', intro: 'button' }
     ]
@@ -9,7 +10,6 @@ export default [
   {
     name: 'popup类组件',
     item: [
-      {name: '遮罩', intro: 'backdrop'},
       {name: '弹出框', intro: 'alert'},
       {name: '加载框', intro: 'loading'},
       {name: '指示器', intro: 'indicator'}
